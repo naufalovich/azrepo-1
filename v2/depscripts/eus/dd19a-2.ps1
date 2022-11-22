42514b9effd15869a0680e589a7a7fd008a0e882
sayfuladrian <sayful.adrian@gmail.com>
2022-11-22 11:10:04 +0700

    add datadisk & snapshot

---
+++ v2/depscripts/eus/dd19a-2.ps1
@@ -0,0 +1,26 @@
+$var = $lc + "ddad19a" + "-2"
+
+Write-Output `n "======================================================================================" 
+Write-Output    "Creating  $var <======================================================================" 
+Write-Output    "======================================================================================" `n
+
+$check = Get-AzDisk -Name $var -ErrorAction SilentlyContinue
+
+if($check -eq $null){
+
+	New-AzResourceGroupDeployment `
+	  -Name remoteTemplateDeployment `
+	  -ResourceGroupName $rg `
+	  -TemplateUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/templates/datadisk.json" `
+	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/$var.json"
+	  
+}
+
+else{
+
+    Write-Host "$var already exist"
+
+}
+
+
+Write-Output `n "Completed $var <======================================================================" `n
--- v2/depscripts/ine.ps1
+++ v2/depscripts/ine.ps1
@@ -1,46 +1,16 @@
-Write-Output `n "======================================================================================" 
-Write-Output    "===============================  Set IPv4  to Static  ================================" 
-Write-Output    "======================================================================================" `n
+Write-Output `n "================================ ENVIRONMENT CREATION ================================" `n
 
-$IP = "10.0.1.12"
-$MaskBits = 24 # This means subnet mask = 255.255.255.0
-$Gateway = "10.0.1.1"
-$Dns = @('10.0.1.11','11.0.1.11')
-$IPType = "IPv4"
+$lc = "eus" + "-"
+$rg = $lc + "rg"
+$loc = "eastus"
 
-# Retrieve the network adapter that you want to configure
-$adapter = Get-NetAdapter | ? {$_.Status -eq "up"}
+#.\eus\dd19a-1.ps1
+#.\eus\dd19a-2.ps1
+#.\eus\dd19a-3.ps1
+#.\eus\dd19a-4.ps1
 
-# Remove any existing IPgateway from our ipv4 adapter
-If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
- $adapter | Remove-NetIPAddress -AddressFamily $IPType -Confirm:$false
-}
+$dataDiskName = "eus-ddad19a-1"
+$dataDisk1 = Get-AzDisk -Name $dataDiskName -ResourceGroupName $rg
+$vm = Add-AzVMDataDisk -VM eus-vmad19a -Name $dataDiskName -CreateOption Attach -ManagedDiskId $dataDisk1.Id -Lun 1
 
-If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
- $adapter | Remove-NetRoute -AddressFamily $IPType -Confirm:$false
-}
-
-# Configure the IP address and default gateway
-$adapter | New-NetIPAddress `
- -AddressFamily $IPType `
- -IPAddress $IP `
- -PrefixLength $MaskBits `
- -DefaultGateway $Gateway
-
-# Configure the DNS client server IP addresses
-$adapter | Set-DnsClientServerAddress -ServerAddresses $DNS
-
-Write-Output `n "======================================================================================" 
-Write-Output    "===================================  Join Domain  ====================================" 
-Write-Output    "======================================================================================" `n
-
-$dc = "minilico.xyz" # Specify the domain to join.
-$pw = "Jakarta@2022" | ConvertTo-SecureString -asPlainText –Force # Specify the password for the domain admin.
-$usr = "$dc\rian" # Specify the domain admin account.
-$creds = New-Object System.Management.Automation.PSCredential($usr,$pw)
-Add-Computer -DomainName $dc -Credential $creds -restart -force -verbose 
-# Note that the computer will be restarted automatically.
-
-Write-Output `n "======================================================================================" 
-Write-Output    "=========================  COMPLETE | COMPLETE | COMPLETE  ===========================" 
-Write-Output    "======================================================================================" `n
+Update-AzVM -VM $vm -ResourceGroupName $rg
