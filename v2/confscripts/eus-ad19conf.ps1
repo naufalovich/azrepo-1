3b1600682eb5ac23069b3bddda6b2d5d54d1cbed
sayfuladrian <sayful.adrian@gmail.com>
2022-12-08 09:20:16 +0700

    put main AD19 configuration to one

--- v2/confscripts/eus-ad19conf.ps1
+++ v2/confscripts/eus-ad19conf.ps1
@@ -27,42 +27,75 @@ Write-Output    "===============================================================
 $LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
 
 Write-Output `n "======================================================================================" 
-Write-Output    "================================  Configure AD & DNS  ================================" 
-Write-Output    "======================================================================================" `n
-
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/confscripts/eus-ad19aconf.xml" -OutFile 'C:\T\addns.xml'
-#Install-WindowsFeature -ConfigurationFilePath 'C:\T\addns.xml'
-
-Write-Output `n "======================================================================================" 
-Write-Output    "================================  Install Powershell  ================================" 
+Write-Output    "===============================  Configure Powershell  ===============================" 
 Write-Output    "======================================================================================" `n
 
 Install-PackageProvider -Name NuGet -Force
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
+
+Write-Output `n "======================================================================================" 
+Write-Output    "===============================  Install Module ADDS  ================================" 
+Write-Output    "======================================================================================" `n
+
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+Write-Output `n "======================================================================================" 
+Write-Output    "===============================  Install Module Azure  ===============================" 
+Write-Output    "======================================================================================" `n
+
 Register-PSRepository -Default
 Install-Module -Name Az -Force
 
+Write-Output `n "======================================================================================" 
+Write-Output    "==============================  Download Required Apps  ==============================" 
+Write-Output    "======================================================================================" `n
+
 New-Item -ItemType Directory -Force -Path C:\T\
 
+#Powershell 7.3.0 + installation
 $URL = "https://github.com/PowerShell/PowerShell/releases/download/v7.3.0/PowerShell-7.3.0-win-x64.msi"
 $Path= "C:\T\PowerShell7.3.msi"
 (New-Object System.Net.WebClient).DownloadFile($URL, $Path)
 msiexec.exe /package "C:\T\PowerShell7.3.msi" /quiet /l* logps7.3.txt ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
+Remove-Item "C:\T\PowerShell7.3.msi" -Force
 
+#StorageSyncAgent + installation
 $URL = "https://download.microsoft.com/download/1/8/D/18DC8184-E7E2-45EF-823F-F8A36B9FF240/StorageSyncAgent_WS2019.msi"
 $Path= "C:\T\StorageSync2019.msi"
 (New-Object System.Net.WebClient).DownloadFile($URL, $Path)
+msiexec.exe /package "C:\T\StorageSync2019.msi" /quiet
+Remove-Item "C:\T\StorageSync2019.msi" -Force
 
+#WindowsAdminCenter
 $URL = "https://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter2110.2.msi"
 $Path= "C:\T\WindowsAdminCenter2110.msi"
 (New-Object System.Net.WebClient).DownloadFile($URL, $Path)
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/confscripts/eus-ad19aconf2.ps1" -OutFile 'C:\T\eus-ad19aconf2.ps1'
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/confscripts/eus-ad19aconf3.ps1" -OutFile 'C:\T\eus-ad19aconf3.ps1'
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/confscripts/eus-ad19aconf4.ps1" -OutFile 'C:\T\eus-ad19aconf4.ps1'
+Write-Output `n "======================================================================================" 
+Write-Output    "===========================  Disable IE Enhanced Security  ===========================" 
+Write-Output    "======================================================================================" `n
+
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+Stop-Process -Name Explorer
+Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+
+Write-Output `n "======================================================================================" 
+Write-Output    "===============================  Set Default Browser  ================================" 
+Write-Output    "======================================================================================" `n
 
 Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\ftp\UserChoice' -name ProgId ChromeHTML
 Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice' -name ProgId ChromeHTML
 Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice' -name ProgId ChromeHTML
+
+Write-Output `n "======================================================================================" 
+Write-Output    "=========================  Download Configuration Files ==============================" 
+Write-Output    "======================================================================================" `n
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/confscripts/eus-ad19aconf2.ps1" -OutFile 'C:\T\eus-ad19aconf2.ps1'
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/confscripts/eus-ad19aconf3.ps1" -OutFile 'C:\T\eus-ad19aconf3.ps1'
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/confscripts/eus-ad19aconf4.ps1" -OutFile 'C:\T\eus-ad19aconf4.ps1'
